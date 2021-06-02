12707b7dda1f813fc47481fbea5e99e9e7c59b44
Vadym Trykhlib <vadimtrihleb@gmail.com>
2021-06-02 23:35:04 +0300

    using express router

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 // Load env vars
 dotenv.config({
     path: './config/config.env'
@@ -8,40 +11,8 @@ dotenv.config({
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: 'Show all bootcamps'
-    })
-})
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: `Get bootcamp ${req.params.id}`
-    })
-})
-
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: 'Create new bootcamp'
-    })
-})
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: `Update bootcamp ${req.params.id}`
-    })
-})
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: `Delete bootcamp ${req.params.id}`
-    })
-})
+// Mount routes
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
