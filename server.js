eed6c8d2bc78ea1c1e705dca8c01221d158de808
Vadym Trykhlib <vadimtrihleb@gmail.com>
2021-06-05 05:25:29 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({
     path: './config/config.env'
 });
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+// Connect to DB
+connectDB();
 
 const app = express();
 
@@ -22,6 +26,15 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
+
+// Handle unhandled refections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    // Close server & exit process
+
+    server.close(() => process.exit(1));
+})
