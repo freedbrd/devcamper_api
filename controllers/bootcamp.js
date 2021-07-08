a351ed247d6061ca6b4a89ba11dfbbbb36630685
Vadym Trykhlib <vadimtrihleb@gmail.com>
2021-07-08 23:15:29 +0300

    create bootcamp

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamps');
+
 /**
  * @description Get all bootcamps
  * @route       GET /api/v1/bootcamps
@@ -27,11 +29,27 @@ exports.getBootcamp = (req, res, next) => {
  * @route       POST /api/v1/bootcamps
  * @access      Private
  */
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        message: 'Create new bootcamp'
-    })
+exports.createBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.create(req.body);
+
+        res.status(201)
+            .json({
+                success: true,
+                data: bootcamp
+            });
+    } catch (err) {
+
+        console.log(err);
+
+        res.status(400)
+            .json({
+                success: false
+            })
+    }
+
+
+
 }
 
 /**
